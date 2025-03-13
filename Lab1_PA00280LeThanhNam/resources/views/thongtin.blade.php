10cb3426643be1a59142cbc0315c82f0b1e99c1f
namle118205 <nle61144@gmail.com>
2025-03-13 20:33:13 +0700

    bootstrap lại cho phần thông tin sinh viên

--- Lab1_PA00280LeThanhNam/resources/views/thongtin.blade.php
+++ Lab1_PA00280LeThanhNam/resources/views/thongtin.blade.php
@@ -5,12 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Giới thiệu bản thân</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Giới thiệu bản thân</h1>
-    <p>Họ tên: Lê Thành Nam</p>
-    <p>MSSV: PA00280</p>
-    <p>Email: nle61144@gmail.com</p>
-    <p>Sở thích: Đọc truyện</p>
+    <div class="container mt-5">
+        <div class="card shadow-sm">
+            <div class="card-header bg-primary text-white text-center py-4">
+                <h1 class="mb-0">Giới thiệu bản thân</h1>
+            </div>
+            <div class="card-body p-4">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><strong>Họ tên:</strong> Lê Thành Nam</li>
+                    <li class="list-group-item"><strong>MSSV:</strong> PA00280</li>
+                    <li class="list-group-item"><strong>Email:</strong> nle61144@gmail.com</li>
+                    <li class="list-group-item"><strong>Sở thích:</strong> Đọc truyện</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
